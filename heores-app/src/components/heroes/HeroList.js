c1f8aa2b0a93891252bb2de3027c4f7ac5645837
Jime Pardo <jimenapardo@outlook.com>
2021-01-20 20:53:31 -0300

    heroes'cards

--- heores-app/src/components/heroes/HeroList.js
+++ heores-app/src/components/heroes/HeroList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ( {publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+            <div className="row" data-masonry='{"percentPosition": true }'>
             {
                 heroes.map( hero => (
-                    <li key={ hero.id }>
-                        { hero.superhero }
-                    </li>
+                    <HeroCard 
+                        key={ hero.id }
+                            {...hero }
+                     />
                 ))
             }
-        </ul>
+            </div>
     )
 }
