a5ae481a287cb09bbb16b9ae58db505ca196905f
Jime Pardo <jimenapardo@outlook.com>
2021-01-20 16:40:33 -0300

    crear router

---
+++ heores-app/src/HeroesApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppRouter } from './routers/AppRouter';
+
+export const HeroesApp = () => {
+    return (
+        <div>
+            <AppRouter />
+
+        </div>
+    )
+}
---
+++ heores-app/src/routers/AppRouter.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { LoginScreen } from '../components/login/LoginScreen';
+import { Marvel } from '../components/marvel/Marvel';
+
+import {Navbar} from '../components/marvel/ui/Navbar';
+
+
+export const AppRouter = () => {
+    return (
+        <Router>
+        <div>
+            <Navbar />
+
+
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/" component={Marvel} />
+         
+        </Switch>
+      </div>
+    </Router>
+    )
+}
