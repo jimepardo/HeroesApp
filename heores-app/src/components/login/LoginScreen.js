f48ffb3252fb97a047a689ddfb2a1d83f13265a8
Jime Pardo <jimenapardo@outlook.com>
2021-01-20 17:15:13 -0300

    history replace

--- heores-app/src/components/login/LoginScreen.js
+++ heores-app/src/components/login/LoginScreen.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+
+    const handleLogin = () => {
+        // history.push('/');
+        history.replace('/');
+    }
+
     return (
-        <div>
-            <h1>LoginScreen</h1>
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={handleLogin}
+            >
+                Ingresar
+            </button>
         </div>
     )
 }
