238633ad6a4ff9c48bbe382bca307c481ccd92da
Jime Pardo <jimenapardo@outlook.com>
2021-01-21 03:58:24 -0300

    Private routes

--- heores-app/src/HeroesApp.js
+++ heores-app/src/HeroesApp.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';
 import { AppRouter } from './routers/AppRouter';
+import { authReducer } from './auth/authReducer';
+import { AuthContext } from './auth/AuthContext';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('user')) || {logged: false};
+}
 
 export const HeroesApp = () => {
+
+    const [ user, dispatch ] = useReducer(authReducer, {}, init);
+
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user));
+    }, [user])
+
     return (
-        <div>
+       <AuthContext.Provider value={{user, dispatch }}>
+
             <AppRouter />
 
-        </div>
+        </AuthContext.Provider>
     )
 }
